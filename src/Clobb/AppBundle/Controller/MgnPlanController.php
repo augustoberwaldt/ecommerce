1da6dc336de47d47bef081a48ab3a3ded547dd8e
augusto <augusto.berwaldt@gmail.com>
2015-03-03 14:14:35 -0300

    termos epalnos

---
+++ src/Clobb/AppBundle/Controller/MgnPlanController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Clobb\AppBundle\Controller;
+
+namespace Clobb\AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use FOS\RestBundle\Controller\FOSRestController,
+    FOS\RestBundle\View\View,
+    FOS\RestBundle\View\ViewHandler,
+    FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Util\Codes;
+use Clobb\AppBundle\Controller\CoreController;
+use Clobb\AppBundle\Entity\MgnPlan;
+use Clobb\AppBundle\Form\MgnPlanType;
+
+/**
+ * MgnPlan controller.
+ *
+ * @Route("/mgnplan")
+ */
+class MgnPlanController extends CoreController
+{
+
+    /**
+     * Lists all MgnPlan.
+     *
+     * @ApiDoc(
+     *     section = "5- Cadastro de Planos",
+     *     statusCodes={
+     *         200="Returned when successful",
+     *     }
+     * )
+     * @Rest\Get()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('PortotechAppBundle:MgnPlan')->findAllPlans();
+
+        return $this->view($entities);
+    }
+
+    /**
+     * busca um Plano.
+     *
+     * @ApiDoc(
+     *     section = "5- Cadastro de Planos",
+     *     statusCodes={
+     *         200="Returned when successful",
+     *     }
+     * )
+     * @Rest\Get("/get/{id}")
+     */
+
+    public function getAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('PortotechAppBundle:Planos')->find($id);
+
+        return $this->view($entities);
+    }
+    /**
+     * Update  Plano.
+     *
+     * @ApiDoc(
+     *     section = "5- Cadastro de Planos",
+     *     statusCodes={
+     *         200="Returned when successful",
+     *     }
+     * )
+     * @Rest\Put("/update/{id}")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PortotechAppBundle:Planos')->find($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Plano entity.');
+        }
+
+    }
+    /**
+     * Delete  Plano.
+     *
+     * @ApiDoc(
+     *     section = "5- Cadastro de Planos",
+     *     statusCodes={
+     *         200="Returned when successful",
+     *     }
+     * )
+     * @Rest\Delete("/delete/{id}")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('PortotechAppBundle:Planos')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Planos entity.');
+        }
+
+        $entity->setDataMotificacao(new \DateTime(date('Y-m-d H:i:s')));
+        $entity->setStatus(0);
+        $em->flush();
+
+        return $this->view(null, Codes::HTTP_NO_CONTENT);
+    }
+
+}
